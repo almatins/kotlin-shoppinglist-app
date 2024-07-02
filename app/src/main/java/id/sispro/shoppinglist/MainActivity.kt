f27eb0505d504281cafe7c955b3647e4ab1518f8
Bono Costa <bh@localhost.localdomain>
2024-07-02 20:08:45 +0700

    restart

--- app/src/main/java/id/sispro/shoppinglist/MainActivity.kt
+++ app/src/main/java/id/sispro/shoppinglist/MainActivity.kt
@@ -3,7 +3,7 @@ package id.sispro.shoppinglist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ShoppingActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
